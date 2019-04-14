93845b33e0a652366fc5a538537ef11ff6f08d6b
omega-coder <fy_cherief@esi.dz>
2019-04-14 04:44:45 +0200

    Adds check_signature method and makes MAGIC_NUMBER__ to class attrib

--- omegalib/networking/btsnoop.py
+++ omegalib/networking/btsnoop.py
@@ -2,16 +2,33 @@
 
 # coding : utf-8
 
+import struct
 
-MAGIC_NUMBER__ = ['\x62', '\x74', '\x73', '\x6e', '\x6f', '\x6f', '\x70', '\x00']
 
 class BTSnoop(object):
+    MAGIC_NUMBER__ = b'\x62\x74\x73\x6e\x6f\x6f\x70\x00'
+
     def __init__(self, file_path):
         try:
             btsnoop_dump = open(file_path, 'rb')
             self.data = btsnoop_dump.read()
         except IOError, (errorno, strerror):
             print("I/O Error({}): {}".format(errorno, strerror))
+
+        if !self.__check_signature():
+            raise Exception
+
+    # check file signature
+    def __check_signature(self):
+        return self.data[:8] == BTSnoop.MAGIC_NUMBER__
+
+    # dump raw binary data
+    def dump_raw(self):
+        return self.data
+
+    
+
+
 
 
 
