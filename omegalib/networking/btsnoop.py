887333f8233909f5712d892b539cf0b620947965
omega-coder <fy_cherief@esi.dz>
2019-04-14 16:41:04 +0200

    Transfer __header_size to class attributes

--- omegalib/networking/btsnoop.py
+++ omegalib/networking/btsnoop.py
@@ -8,8 +8,16 @@ import struct
 class BTSnoop(object):
     MAGIC_NUMBER__ = b'\x62\x74\x73\x6e\x6f\x6f\x70\x00'
 
+    BTSNOOP_FLAGS =  {
+        0: ("host", "controller", "data"),
+        1: ("controller", "host", "data"),
+        2: ("host", "controller", "command"),
+        3: ("controller", "host", "event")
+    }
+    __header_size = struct.calcsize('>II')
+
+
     def __init__(self, file_path):
-        self.__header_size = struct.calcsize('>II')
         self.__version = None
         self.__datalink_type = None
         try:
@@ -20,6 +28,10 @@ class BTSnoop(object):
 
         if not self.__check_signature():
             raise Exception
+        try:
+            self.__parse_header()
+        except Exception as e:
+            print(e)
 
     # check file signature
     def __check_signature(self):
@@ -29,11 +41,14 @@ class BTSnoop(object):
     def dump_raw(self):
         return self.data
 
-    def read_header(self):
+    def __parse_header(self):
         magic_number_len = len(MAGIC_NUMBER__)
-        self.__version, self.__datalink_type = struct.unpack('>II', self.data[magic_number_len:magic_number_len+self.__header_size])
+        self.__version, self.__datalink_type = struct.unpack('>II', self.data[magic_number_len:magic_number_len+__header_size])
         if self.__version != 1:
             raise Exception('Version {} is not supported!'.format(self.__version))
+        if self.__datalink_type != 0x3ea:
+            raise Exception('Only H4 Datalink type is supported!')
+
 
 
 
